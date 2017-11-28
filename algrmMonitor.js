d1ee02f2bdc69f2a916634e96dcc49c1f1dcab22
OHggsHntr <BusuZube6>
2017-11-29 00:51:35 +0200

    minor changes. mainly cosmetics. also add username to table

--- algrmMonitor.js
+++ algrmMonitor.js
@@ -1,107 +1,102 @@
-var algrmServerURL = "http://127.0.0.1:4000/monitor/";
+(function() {
+	var algrmServerURL = "/monitor/";
 
 
-function gpuPanel(gpu) {
-	this.gpu = gpu;
+	function gpuPanel(gpu) {
+		this.gpu = gpu;
 
-	this.render = function() {
-		var html = '';
-		html += '<h1 id="gpuPanelTitle_' + gpu.internalIndex + '" class="gpu-title">';
-		html += this.gpu.gpuName + ' (' + gpu.gpuIdx + ')';
-		html += '</h1>'
-		html += '<div id="gpuUtilGraphPanel_' + gpu.internalIndex + '" class="gpu-graph">';
-		html += '</div>'
-		html += '<div id="gpuMemGraphPanel_' + gpu.internalIndex + '" class="gpu-graph">';
-		html += '</div>'
-		html += '<div id="gpuProcessPanel_' + gpu.internalIndex + '" class="gpu-process">';
-		html += '</div>'
-		document.getElementById("gpuPanel_" + gpu.internalIndex).innerHTML = html;
-	}
-	
-	this.updateGPUInfo = function(gpuInfo) {
-		var html = '';
-		html += '<table id="gpuProcessTable_' + this.gpu.internalIndex + '" class="gpu-process-table">';
-		html += '<tr><th>PID</th><th>Process</th><th>Mem Usage [MB]</th></tr>';
-		for (i in gpuInfo.procs) {
-			var usedGpuMemoryPercent = 100*gpuInfo.procs[i].usedGpuMemory/gpuInfo.memTotal + '%';
-			var usedGpuMemory = gpuInfo.procs[i].usedGpuMemory/1024/1024 + ' (' + usedGpuMemoryPercent + '%)';
-			if (gpuInfo.procs[i].usedGpuMemory<0) {
-				usedGpuMemory = "?";
-			}
-			else {
-			}
-			html += '<tr>';
-			html += '<td>' + gpuInfo.procs[i].pid + '</td>';
-			html += '<td>' + gpuInfo.procs[i].cmd + '</td>';
-			html += '<td>' + usedGpuMemory + '</td>';
-			html += '</tr>';
+		this.render = function() {
+			var html = '<h1 id="gpuPanelTitle_<id>" class="gpu-title"><gpuName> (<gpuIdx>)</h1> \
+			<div id="gpuUtilGraphPanel_<id>"  class="gpu-graph"></div> \
+			<div id="gpuMemGraphPanel_<id>"  class="gpu-graph"></div> \
+			<div id="gpuProcessPanel_<id>"  class="gpu-process"></div>'.replace(/<id>/g, gpu.internalIndex)
+			.replace('<gpuName>', this.gpu.gpuName).replace('<gpuIdx>', gpu.gpuIdx);
+			document.getElementById("gpuPanel_" + gpu.internalIndex).innerHTML = html;
 		}
-		html += '</table>';
-		document.getElementById("gpuProcessPanel_" + this.gpu.internalIndex).innerHTML = html;
+		
+		this.updateGPUInfo = function(gpuInfo) {
+			var html = '<table id="gpuProcessTable_<id>" class="gpu-process-table">\
+			<tr><th>PID</th><th>Process</th><th>User</th><th>Mem Usage [MB]</th></tr><tablebody></table>'
+			.replace(/<id>/g, gpu.internalIndex);
+			var tablebody = '';
+			for (i in gpuInfo.procs) {
+				var usedGpuMemoryPercent = 100*gpuInfo.procs[i].usedGpuMemory/gpuInfo.memTotal;
+				var usedGpuMemory = gpuInfo.procs[i].usedGpuMemory/1024/1024 + ' (' + usedGpuMemoryPercent + '%)';
+				if (gpuInfo.procs[i].usedGpuMemory<0) {
+					usedGpuMemory = "?";
+				}
+				else {
+				}
+				tablebody += '<tr><td><pid></td><td><cmd></td><td><username></td><td><mem></td></tr>'
+				.replace('<pid>', gpuInfo.procs[i].pid).replace('<cmd>', gpuInfo.procs[i].cmd)
+				.replace('<username>', gpuInfo.procs[i].username).replace('<mem>', usedGpuMemory);
+			}
+			html = html.replace('<tablebody>', tablebody);
+			document.getElementById("gpuProcessPanel_" + this.gpu.internalIndex).innerHTML = html;
+		}
+
+		this.refresh = function() {
+			var xhttp = new XMLHttpRequest();
+			xhttp.panel = this;
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					// if panel was freezed, unfreeze.
+					var gpuInfo = JSON.parse(this.responseText);
+					this.panel.updateGPUInfo(gpuInfo);
+				} else if ((this.readyState == 4 && this.status == 0) || this.status >= 400) {
+					console.log("failed to get GPU info: algrm server is not responding");
+					// Should indicate panel is freeze
+				}
+			} ;
+			xhttp.open("GET", algrmServerURL + "GPUs?gpuIdx=" + this.gpu.gpuIdx, true);
+			xhttp.send();
+		}
 	}
 
-	this.refresh = function() {
+
+	function constructGPUPanels(gpus) {
+		var panels = []
+		var html = ''
+		for (i in gpus) {
+			console.log("gpu " + gpus[i].gpuIdx + ": " + gpus[i].gpuName + "(" + gpus[i].gpuId + ")");
+			var gpu = gpus[i];
+			gpu.internalIndex = i;
+			html += '<div id="gpuPanel_' + gpu.internalIndex + '" class="gpu-panel">';
+			html += '</div>'
+			var panel = new gpuPanel(gpu);
+			panels[i] = panel;
+		}
+		document.getElementById("algrmDashboard").innerHTML = html;
+		
+		for (i in panels) {
+			panels[i].render();
+		}
+		
+		setInterval(function() {
+			for (i in panels) {
+				panels[i].refresh();
+			}
+		}, 1000);
+	}
+
+
+
+	function buildAlgrmDashboard(algrmServerURL) {
 		var xhttp = new XMLHttpRequest();
-		xhttp.panel = this;
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
-				// if panel was freezed, unfreeze.
-				var gpuInfo = JSON.parse(this.responseText);
-				this.panel.updateGPUInfo(gpuInfo);
-			} else if ((this.readyState == 4 && this.status == 0) || this.status >= 400) {
-				console.log("failed to get GPU info: algrm server is not responding");
-				// Should indicate panel is freeze
+				var gpus = JSON.parse(this.responseText);
+				constructGPUPanels(gpus);
+			} else if ((this.readyState == 4 && this.status == 0) ||this.status >= 400) {
+				console.log("failed to load GPUs: algrm server is not responding");
+				document.getElementById("algrmDashboard").innerHTML = 
+					"<div class='errorAlgrmLine'>R.I.P.<BR>algrm is dead</div>";
 			}
 		} ;
-		xhttp.open("GET", algrmServerURL + "GPUs?gpuIdx=" + this.gpu.gpuIdx, true);
+		xhttp.open("GET", algrmServerURL + "GPUs", true);
 		xhttp.send();
 	}
-}
 
 
-function constructGPUPanels(gpus) {
-	var panels = []
-	var html = ''
-	for (i in gpus) {
-		console.log("gpu " + gpus[i].gpuIdx + ": " + gpus[i].gpuName + "(" + gpus[i].gpuId + ")");
-		var gpu = gpus[i];
-		gpu.internalIndex = i;
-		html += '<div id="gpuPanel_' + gpu.internalIndex + '" class="gpu-panel">';
-		html += '</div>'
-		var panel = new gpuPanel(gpu);
-		panels[i] = panel;
-	}
-	document.getElementById("algrmDashboard").innerHTML = html;
-	
-	for (i in panels) {
-		panels[i].render();
-	}
-	
-	setInterval(function() {
-		for (i in panels) {
-			panels[i].refresh();
-		}
-	}, 1000);
-}
-
-
-
-function buildAlgrmDashboard(algrmServerURL) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var gpus = JSON.parse(this.responseText);
-			constructGPUPanels(gpus);
-		} else if ((this.readyState == 4 && this.status == 0) ||this.status >= 400) {
-			console.log("failed to load GPUs: algrm server is not responding");
-			document.getElementById("algrmDashboard").innerHTML = 
-				"<div class='errorAlgrmLine'>R.I.P.<BR>algrm is dead</div>";
-		}
-	} ;
-	xhttp.open("GET", algrmServerURL + "GPUs", true);
-	xhttp.send();
-}
-
-
-buildAlgrmDashboard(algrmServerURL);
-
+	buildAlgrmDashboard(algrmServerURL);
+})();
--- algrm_server.py
+++ algrm_server.py
@@ -1,17 +1,41 @@
 from flask import Flask
 from flask import request
-from flask_cors import CORS
 import psutil
 import argparse
+import threading
+import time
+
+
+TIME_INTERVAL = 1
+
 
 SIMULATE_CUDA = True
 if not SIMULATE_CUDA:
-    from pynvml import *
+    from py3nvml.py3nvml import *
 
+app = Flask(__name__, static_folder='C:\\inetpub\\wwwroot\\ohad')
 
-app = Flask(__name__)
-CORS(app)
+lock = threading.Lock()
 
+class History:
+    def __init__(self):
+        self.MAX_TIME_POINTS = 60*60*24
+        self.time = list()
+        self.util_history = list()
+        self.mem_history = list()
+
+    def add(self, t, util, mem):
+        self.time.append(t)
+        self.util_history.append(util)
+        self.mem_history.append(mem)
+        if len(self.time) > self.MAX_TIME_POINTS:
+            self.time = self.time[1:]
+            self.util_history = self.util_history[1:]
+            self.mem_history = self.mem_history[1:]
+        pass
+    pass
+
+devices_history = list()
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Resource Manager')
@@ -19,19 +43,43 @@ def parse_args():
     return parser.parse_args()
 
 
+def monitor_daemon():
+    while True:
+        time.sleep(TIME_INTERVAL)
+        t = time.time()
+        device_count = nvmlDeviceGetCount()
+        all_utils = list()
+        all_mems = list()
+        for i in range(device_count):
+            handle = nvmlDeviceGetHandleByIndex(i)
+            util = nvmlDeviceGetUtilizationRates(handle)
+            all_utils.append(util.gpu)
+            all_mems.append(util.memory)
+        lock.acquire()
+        for i in range(device_count):
+            history = devices_history[i]
+            history.add(t, all_utils[i], all_mems[i])
+        lock.release()
+    pass
+
+
 def main():
     args = parse_args()
     app.debug = False
     if not SIMULATE_CUDA:
         nvmlInit()
+        for _ in range(nvmlDeviceGetCount()):
+            devices_history.append(History())
+        t = threading.Thread(name='monitor-daemon', target=monitor_daemon, daemon=True )
+        t.start()
     app.run(port=args.port)
 
 
-@app.route('/monitor/GPUs', methods=['GET', 'POST'])
-def monitor_gpus():
+def monitor_all_gpus():
     if SIMULATE_CUDA:
-        return '[{"gpu_i":0, "gpu_id":"0000:01:00.0", "gpu_name":"GeForce GTX 1050"}, \
-        {"gpu_i":1, "gpu_id":"0000:02:00.0", "gpu_name":"GeForce TITAN-X"}]'
+        return '[{"gpuIdx":0, "gpuId":"0000:01:00.0", "gpuName":"GeForce GTX 1050", "memTotal":1711276032}, \
+        {"gpuIdx":1, "gpuId":"0000:02:00.0", "gpuName":"GeForce TITAN-X", "memTotal":855638016}]'
+
     deviceCount = nvmlDeviceGetCount()
     ret_json = '['
     for i in range(deviceCount):
@@ -39,22 +87,89 @@ def monitor_gpus():
         handle = nvmlDeviceGetHandleByIndex(i)
         pciInfo = nvmlDeviceGetPciInfo(handle)
         gpu_id = pciInfo.busId.decode("utf-8")
-        gpu_name = nvmlDeviceGetName(handle).decode("utf-8")
-        # tx_bytes = nvmlDeviceGetPcieThroughput(handle, NVML_PCIE_UTIL_TX_BYTES)
-        # rx_bytes = nvmlDeviceGetPcieThroughput(handle, NVML_PCIE_UTIL_RX_BYTES)
-        util = nvmlDeviceGetUtilizationRates(handle)
-        gpu_util = util.gpu
-        mem_util = util.memory
-        temp = nvmlDeviceGetTemperature(handle, NVML_TEMPERATURE_GPU)
-        gpu_clock = nvmlDeviceGetClockInfo(handle, NVML_CLOCK_GRAPHICS)
-        gpu_clock_max = nvmlDeviceGetMaxClockInfo(handle, NVML_CLOCK_GRAPHICS)
-        gpu_mem_clock = nvmlDeviceGetClockInfo(handle, NVML_CLOCK_MEM)
-        gpu_mem_clock_max = nvmlDeviceGetMaxClockInfo(handle, NVML_CLOCK_SM)
-        procs = nvmlDeviceGetComputeRunningProcesses(handle)
-        ret_json += f'"gpu_i":{i}, "gpu_id":"{gpu_id}", "gpu_name":"{gpu_name}"'
+        gpu_name = nvmlDeviceGetName(handle)
+        mem_info = nvmlDeviceGetMemoryInfo(handle)
+        ret_json += f'"gpuIdx":{i}, "gpuId":"{gpu_id}", "gpuName":"{gpu_name}", "memTotal":{mem_info.total}'
         ret_json += '},'
     ret_json = ret_json[:-1] + ']'
     return ret_json
+
+
+def monitor_device(gpu_idx):
+    if SIMULATE_CUDA:
+        if gpu_idx == 0:
+            mem_total = 1711276032
+            mem_free = 0
+            mem_used = 1711276032
+            gpu_util = 72
+            procs = '[{"pid":1438, "usedGpuMemory":427819008, "cmd":"python.exe"}, {"pid":1430, "usedGpuMemory":1283457024, "cmd":"win32.exe"}]'
+        elif gpu_idx == 1:
+            mem_total = 855638016
+            mem_free = 855638016
+            mem_used = 0
+            gpu_util = 0
+            procs = '[]'
+        else:
+            return ''
+        return '{' + f'"memTotal":{mem_total}, "memFree":{mem_free}, "memUsed":{mem_used}, "gpuUtil":{gpu_util}, "procs":{procs}' + '}'
+
+    try:
+        handle = nvmlDeviceGetHandleByIndex(gpu_idx)
+        procs = nvmlDeviceGetComputeRunningProcesses(handle)
+
+        mem_info = nvmlDeviceGetMemoryInfo(handle)
+        util = nvmlDeviceGetUtilizationRates(handle)
+        gpu_util = util.gpu
+        mem_util = util.memory
+        # temp = nvmlDeviceGetTemperature(handle, NVML_TEMPERATURE_GPU)
+        # gpu_clock = nvmlDeviceGetClockInfo(handle, NVML_CLOCK_GRAPHICS)
+        # gpu_clock_max = nvmlDeviceGetMaxClockInfo(handle, NVML_CLOCK_GRAPHICS)
+        # gpu_mem_clock = nvmlDeviceGetClockInfo(handle, NVML_CLOCK_MEM)
+        # gpu_mem_clock_max = nvmlDeviceGetMaxClockInfo(handle, NVML_CLOCK_SM)
+        # tx_bytes = nvmlDeviceGetPcieThroughput(handle, NVML_PCIE_UTIL_TX_BYTES)
+        # rx_bytes = nvmlDeviceGetPcieThroughput(handle, NVML_PCIE_UTIL_RX_BYTES)
+        ret_json = '{'
+        ret_json += f'"memTotal":{mem_info.total}, "memFree":{mem_info.free}, "memUsed":{mem_info.used},'
+        ret_json += f'"gpuUtil":{gpu_util},'
+        ret_json += '"procs":['
+        for p in procs:
+            proc = psutil.Process(p.pid)
+            usedGpuMemory = {None:-1}.get(p.usedGpuMemory, p.usedGpuMemory)
+            cmd = proc.name()
+            try:
+                user_name = proc.username().replace('\\', '/')
+            except:
+                user_name = 'N/A'
+            ret_json += '{'
+            ret_json += f'"pid":{p.pid}, "usedGpuMemory":{usedGpuMemory}, "cmd":"{cmd}", "username":"{user_name}"'
+            ret_json += '},'
+        ret_json = ret_json[:-1] + '],'
+        lock.acquire()
+        history = devices_history[gpu_idx]
+        ret_json += f'"graph_time":{history.time[-600:]},'
+        ret_json += f'"graph_util":{history.util_history[-600:]},'
+        ret_json += f'"graph_mem":{history.mem_history[-600:]}'
+        lock.release()
+        ret_json += '}'
+        return ret_json
+    except:
+        return ''
+    pass
+
+
+@app.route('/monitor/GPUs', methods=['GET', 'POST'])
+def monitor_gpus():
+    gpu_idx = request.args.get('gpuIdx', default=-1, type=int)
+    if gpu_idx == -1:
+        return monitor_all_gpus()
+    else:
+        return monitor_device(gpu_idx)
+    pass
+
+
+@app.route('/<path:path>')
+def regulat_http(path):
+    return app.send_static_file( path )
 
 
 if __name__ == "__main__":
