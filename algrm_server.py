93aec1dee033acba33a6ca97205e7eebcde59144
ohads <MiaDani24!>
2018-04-10 11:33:02 +0300

    rewrite the monitor

--- algrm_server.py
+++ algrm_server.py
@@ -1,65 +1,78 @@
 from flask import Flask
 from flask import request
-from bisect import bisect
 import psutil
 import argparse
-import threading
 import time
+from collections import deque
+from py3nvml.py3nvml import *
+import copy
 
 
 TIME_INTERVAL = 0.5
 
 
-from py3nvml.py3nvml import *
-
 app = Flask(__name__, static_folder='C:\\algrmMonitor\\')
 
-lock = threading.Lock()
+
+class HistoryObject:
+    """
+    This is a time point of a single gpu state at time t
+    """
+    def __init__(self, t, util, mem, rx, tx):
+        self.t = t
+        self.util = util
+        self.mem = mem
+        self.rx = rx
+        self.tx = tx
+
 
 class History:
+    """
+    The total history of a single gpu
+    """
     def __init__(self):
         self.MAX_TIME_POINTS = round(60/TIME_INTERVAL)
-        self.time = list()
-        self.util_history = list()
-        self.mem_history = list()
-        self.rx_history = list()
-        self.tx_history = list()
+        self.q = deque(maxlen=self.MAX_TIME_POINTS)
+        # self.lock = threading.Lock()
 
-    def add(self, t, util, mem, rx, tx):
-        self.time.append(t)
-        self.util_history.append(util)
-        self.mem_history.append(mem)
-        self.rx_history.append(rx)
-        self.tx_history.append(tx)
-        if len(self.time) > self.MAX_TIME_POINTS:
-            self.time = self.time[1:]
-            self.util_history = self.util_history[1:]
-            self.mem_history = self.mem_history[1:]
-            self.rx_history = self.rx_history[1:]
-            self.tx_history = self.tx_history[1:]
-        pass
+    def add(self, ho):
+        """
+        add HistoryObject
+        :param ho: A HistoryObject
+        """
+        # self.lock.acquire()
+        self.q.append(ho)
+        # self.lock.release()
 
-    def get_sublist(self, arr, ltime):
-        idx = bisect(self.time, ltime)
-        return arr[idx:]
+    def get_from(self, ltime):
+        """
+        get a list of histories, from ltime to now
+        :param ltime: time point to take histories from
+        :return: a list of HistoryObject
+        """
+        ret = list()
+        tmp_q = deque(maxlen=self.MAX_TIME_POINTS)
 
-    def get_time(self, ltime):
-        return self.get_sublist(self.time, ltime)
+        # self.lock.acquire()
+        q = copy.copy(self.q)
+        # self.lock.release()
+        while len(q) > 0:
+            ho = q.pop()
+            tmp_q.append(ho)
+            if ho.t <= ltime:
+                break
 
-    def get_util_history(self, ltime):
-        return self.get_sublist(self.util_history, ltime)
+        while len(tmp_q) > 0:
+            ho = tmp_q.pop()
+            ret.append(ho)
 
-    def get_mem_history(self, ltime):
-        return self.get_sublist(self.mem_history, ltime)
+        return ret
 
-    def get_rx_history(self, ltime):
-        return self.get_sublist(self.rx_history, ltime)
-
-    def get_tx_history(self, ltime):
-        return self.get_sublist(self.tx_history, ltime)
     pass
 
-devices_history = list()
+
+devices_history = list()  # list of histories for all gpus. It holds History object for each gpu
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Resource Manager')
@@ -72,27 +85,18 @@ def monitor_daemon():
         time.sleep(TIME_INTERVAL)
         t = time.time()
         device_count = nvmlDeviceGetCount()
-        all_utils = list()
-        all_mems = list()
-        all_rxs = list()
-        all_txs = list()
+
         for i in range(device_count):
             handle = nvmlDeviceGetHandleByIndex(i)
             util = nvmlDeviceGetUtilizationRates(handle)
             mem_info = nvmlDeviceGetMemoryInfo(handle)
             tx_bytes = nvmlDeviceGetPcieThroughput(handle, NVML_PCIE_UTIL_TX_BYTES)
             rx_bytes = nvmlDeviceGetPcieThroughput(handle, NVML_PCIE_UTIL_RX_BYTES)
-            all_utils.append(util.gpu)
-            all_mems.append(100*mem_info.used/mem_info.total)
-            all_rxs.append(rx_bytes / 1024 / 1024)
-            all_txs.append(tx_bytes / 1024 / 1024)
-            
-        lock.acquire()
-        for i in range(device_count):
-            history = devices_history[i]
-            history.add(t, all_utils[i], all_mems[i], all_rxs[i], all_txs[i])
-        lock.release()
-        
+            devices_history[i].add(HistoryObject(t=t,
+                                                 util=util.gpu,
+                                                 mem=100 * mem_info.used / mem_info.total,
+                                                 rx=rx_bytes / 1024 / 1024,
+                                                 tx=tx_bytes / 1024 / 1024))
     pass
 
 
@@ -102,25 +106,38 @@ def main():
     nvmlInit()
     for _ in range(nvmlDeviceGetCount()):
         devices_history.append(History())
-    t = threading.Thread(name='monitor-daemon', target=monitor_daemon, daemon=True )
+    t = threading.Thread(name='monitor-daemon', target=monitor_daemon, daemon=True)
     t.start()
-    app.run(port=args.port, host='0.0.0.0')
+    app.run(port=args.port, host='0.0.0.0', threaded=True)
 
 
 def monitor_all_gpus():
-    deviceCount = nvmlDeviceGetCount()
-    ret_json = '['
-    for i in range(deviceCount):
-        ret_json += '{'
+    device_count = nvmlDeviceGetCount()
+    ret = list()
+    for i in range(device_count):
         handle = nvmlDeviceGetHandleByIndex(i)
-        pciInfo = nvmlDeviceGetPciInfo(handle)
-        gpu_id = pciInfo.busId.decode("utf-8")
-        gpu_name = nvmlDeviceGetName(handle)
+        pci_info = nvmlDeviceGetPciInfo(handle)
         mem_info = nvmlDeviceGetMemoryInfo(handle)
-        ret_json += f'"gpuIdx":{i}, "gpuId":"{gpu_id}", "gpuName":"{gpu_name}", "memTotal":{mem_info.total}'
-        ret_json += '},'
-    ret_json = ret_json[:-1] + ']'
-    return ret_json
+        ret.append({"gpuIdx": i,
+                    "gpuId": pci_info.busId.decode("utf-8"),
+                    "gpuName": nvmlDeviceGetName(handle),
+                    "memTotal": mem_info.total})
+    return f'{ret}'.replace("'", '"')
+
+
+def add_gpu_history_to_json(jsn, history, ltime):
+    """
+    add the gpu history to the json
+    :param jsn: json
+    :param history: History
+    :param ltime:  last time in the graph
+    :return: the updated json
+    """
+    l = history.get_from(ltime)
+    f = ["graph_time", "graph_util", "graph_mem", "graph_rx", "graph_tx"]
+    for i, v in enumerate(zip(*map(lambda x: (x.t, x.util, x.mem, x.rx, x.tx), l))):
+        jsn[f[i]] = list(v)
+    return jsn
 
 
 def monitor_device(gpu_idx, ltime):
@@ -131,20 +148,17 @@ def monitor_device(gpu_idx, ltime):
         mem_info = nvmlDeviceGetMemoryInfo(handle)
         util = nvmlDeviceGetUtilizationRates(handle)
         gpu_util = util.gpu
-        mem_util = util.memory
+        # mem_util = util.memory
         # temp = nvmlDeviceGetTemperature(handle, NVML_TEMPERATURE_GPU)
         # gpu_clock = nvmlDeviceGetClockInfo(handle, NVML_CLOCK_GRAPHICS)
         # gpu_clock_max = nvmlDeviceGetMaxClockInfo(handle, NVML_CLOCK_GRAPHICS)
         # gpu_mem_clock = nvmlDeviceGetClockInfo(handle, NVML_CLOCK_MEM)
         # gpu_mem_clock_max = nvmlDeviceGetMaxClockInfo(handle, NVML_CLOCK_SM)
-        ret_json = '{'
-        ret_json += f'"memTotal":{mem_info.total}, "memFree":{mem_info.free}, "memUsed":{mem_info.used},'
-        ret_json += f'"gpuUtil":{gpu_util},'
-        ret_json += '"procs":['
 
+        procs_info = list()
         for p in procs:
             proc = psutil.Process(p.pid)
-            usedGpuMemory = {None:-1}.get(p.usedGpuMemory, p.usedGpuMemory)
+            used_gpu_memory = {None: -1}.get(p.usedGpuMemory, p.usedGpuMemory)
             cmd = proc.name()
             tensorboard_port = -1
             try:
@@ -164,23 +178,21 @@ def monitor_device(gpu_idx, ltime):
             except:
                 user_name = 'N/A'
                 tensorboard_port = -1
-            ret_json += '{'
-            ret_json += f'"pid":{p.pid}, "usedGpuMemory":{usedGpuMemory}, "cmd":"{cmd}", "username":"{user_name}", "tensorboard":{tensorboard_port}'
-            ret_json += '},'
-        if ret_json[-1] == ',':
-            ret_json = ret_json[:-1] + '],'
-        else:
-            ret_json += '],'
-        lock.acquire()
-        history = devices_history[gpu_idx]
-        ret_json += f'"graph_time":{history.get_time(ltime)},'
-        ret_json += f'"graph_util":{history.get_util_history(ltime)},'
-        ret_json += f'"graph_mem":{history.get_mem_history(ltime)},'
-        ret_json += f'"graph_rx":{history.get_rx_history(ltime)},'
-        ret_json += f'"graph_tx":{history.get_tx_history(ltime)}'
-        lock.release()
-        ret_json += '}'
-        return ret_json
+            procs_info.append({"pid": p.pid,
+                               "usedGpuMemory": used_gpu_memory,
+                               "cmd": cmd,
+                               "username": user_name,
+                               "tensorboard": tensorboard_port})
+            pass
+
+        ret = {"memTotal": mem_info.total,
+               "memFree": mem_info.free,
+               "memUsed": mem_info.used,
+               "gpuUtil": gpu_util,
+               "procs": procs_info}
+
+        ret = add_gpu_history_to_json(ret, devices_history[gpu_idx], ltime)
+        return f'{ret}'.replace("'", '"')
     except:
         return ''
     pass
