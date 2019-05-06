150d620923fbb095d94a3efa291df36fe2774c52
Ohad Silbert <ohads@algotec.co.il>
2019-05-06 14:12:54 +0300

    fix bug in table refresh. now compare all table fields and not just the pids

--- algrmMonitorApp/src/gpuPanelView.js
+++ algrmMonitorApp/src/gpuPanelView.js
@@ -1,6 +1,68 @@
 var algrmMonitor = algrmMonitor || {};
 
 (function() {
+	var isEqual = function (value, other) {
+
+		// Get the value type
+		var type = Object.prototype.toString.call(value);
+	
+		// If the two objects are not the same type, return false
+		if (type !== Object.prototype.toString.call(other)) return false;
+	
+		// If items are not an object or array, return false
+		if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
+	
+		// Compare the length of the length of the two items
+		var valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
+		var otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
+		if (valueLen !== otherLen) return false;
+	
+		// Compare two items
+		var compare = function (item1, item2) {
+	
+			// Get the object type
+			var itemType = Object.prototype.toString.call(item1);
+	
+			// If an object or array, compare recursively
+			if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+				if (!isEqual(item1, item2)) return false;
+			}
+	
+			// Otherwise, do a simple comparison
+			else {
+	
+				// If the two items are not the same type, return false
+				if (itemType !== Object.prototype.toString.call(item2)) return false;
+	
+				// Else if it's a function, convert to a string and compare
+				// Otherwise, just compare
+				if (itemType === '[object Function]') {
+					if (item1.toString() !== item2.toString()) return false;
+				} else {
+					if (item1 !== item2) return false;
+				}
+	
+			}
+		};
+	
+		// Compare properties
+		if (type === '[object Array]') {
+			for (var i = 0; i < valueLen; i++) {
+				if (compare(value[i], other[i]) === false) return false;
+			}
+		} else {
+			for (var key in value) {
+				if (value.hasOwnProperty(key)) {
+					if (compare(value[key], other[key]) === false) return false;
+				}
+			}
+		}
+	
+		// If nothing failed, return true
+		return true;
+	
+	};
+
 	/* *******************************************************
 	 * This code was inspired from:
 	 * https://www.awwwards.com/build-a-simple-javascript-app-the-mvc-way.html
@@ -203,30 +265,19 @@ var algrmMonitor = algrmMonitor || {};
 			this.svgUtil = this.buildSVG(d3.select("#" + this.gpuUtilGraphSVGId), "Performance", [ this.strs.lineUtilTitle, this.strs.lineMemTitle ], 100);
 			this.svgX = this.buildSVG(d3.select("#" + this.gpuXGraphSVGId), "PCI", [this.strs.lineRxTitle, this.strs.lineTxTitle], 15);
 			this.gaugeTemp = null;
-			this.oldProcessTablePids = null;
+			this.oldProcessTableProcs = null;
 			this.buildGauge("gaugeTemp", d3.select("#" + this.gpuTempGaugeId), "Temp", this.model.getGPUInfo().temperature, this.model.getGPUInfo().temperatureThreshold);
 		},
 
 		processTableWasUpdate: function() {
-			var oldPids = this.oldProcessTablePids;
+			var oldProcs = this.oldProcessTableProcs;
 			var procs = this.model.getGPUDetails().procs;
-			var newPids = procs.map(x => x.pid);
-			newPids.sort();
-			this.oldProcessTablePids = newPids;
-			if (oldPids) {
-				if (newPids.length == oldPids.length) {
-					for (var i in newPids) {
-						if (newPids[i] != oldPids[i]) {
-							return true;
-						}
-					}
-				} else {
-					return true;
-				}
-			} else {
-				return true;
-			}
-			return false;
+			procs.sort(function(a, b) { return a.pid - b.pid; });
+			this.oldProcessTableProcs = procs;
+			if (oldProcs) {
+				return isEqual(oldProcs, procs);
+			} 
+			return true;
 		},
 		
 		buildProcessTable: function() {
